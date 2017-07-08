cb7fd69af73b2a63b03342ba8e31fc15b471a406
Shuchita <ecetechpark@gmail.com>
2017-07-08 22:17:11 +0530

    Vowel-Consonant

---
+++ ideone_TOYG4D.java
@@ -0,0 +1,35 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception,IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter a character");
+		char letter = (char) br.read();
+		switch(letter){
+			case 'a':
+				System.out.println("Vowel");
+				break;
+			case 'e':
+				System.out.println("Vowel");
+				break;
+			case 'i':
+				System.out.println("Vowel");
+				break;
+			case 'o':
+				System.out.println("Vowel");
+				break;
+			case 'u':
+				System.out.println("Vowel");
+				break;
+			default:
+			System.out.println("Consonant");
+		}
+	}
+}
